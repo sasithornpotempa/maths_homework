24a719b6b9b0fe3a998376e913edd7753791193e
sasithornpotempa <sasithornpotempa@gmail.com>
2025-03-05 19:07:43 +0000

    Add 1.c

---
+++ src/1.c
@@ -0,0 +1,12 @@
+int main() {
+    int x = 5;
+    double y = 3.14;
+    char z = 'a';
+    printf("x + y = %f\n", x + y);
+    printf("x - y = %f\n", x - y);
+    printf("x * y = %f\n", x * y);
+    printf("x / y = %f\n", x / y);
+    printf("x %% y = %d\n", x % y);
+    printf("x ^ y = %f\n", pow(x,y));
+    return 0;
+}
