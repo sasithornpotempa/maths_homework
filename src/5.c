4ffd97a82c4d5faa71cd8cdc1bba449c3d4c3596
sasithornpotempa <sasithornpotempa@gmail.com>
2025-03-09 16:47:45 +0000

    Add 5.c

---
+++ src/5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    int sum = num1 + num2;
+    printf("The sum is: %d\n", sum);
+    return 0;
+}
