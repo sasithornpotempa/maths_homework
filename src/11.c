a93b2f614f3ad5d88078f67c80ccc22709e36a44
sasithornpotempa <sasithornpotempa@gmail.com>
2025-03-15 16:20:42 +0000

    Add 11.c

---
+++ src/11.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int num_1 = 5;
+    int num_2 = 3;
+    int result = num_1 + num_2;
+    printf("The sum of %d and %d is %d\n", num_1, num_2, result);
+    return 0;
+}
